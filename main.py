18d90167a999a3399ef754c4cda0b3a9657247e6
sebastian aguilera <aguilerajr2004@gmail.com>
2025-06-05 18:06:01 +0200

    cambio

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from routes.lavado import router as lavado_router
 from routes.empleados import router as empleados_router
 from routes.vehiculos import router as vehiculos_router
 from routes.codigos import router as codigos_router
+from routes.asignar import router as asignar_router
 
 app = FastAPI()
 
@@ -32,3 +33,4 @@ app.include_router(lavado_router)
 app.include_router(empleados_router)
 app.include_router(vehiculos_router)
 app.include_router(codigos_router)
+app.include_router(asignar_router)
