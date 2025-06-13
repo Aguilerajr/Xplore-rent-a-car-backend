58b1407b9a0ec25b9b00a5fb4e11019814d58bb8
sebastian aguilera <aguilerajr2004@gmail.com>
2025-06-14 00:13:00 +0200

    cambio

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from routes.lavado import router as lavado_router
 from routes.empleados import router as empleados_router
 from routes.vehiculos import router as vehiculos_router
 from routes.codigos import router as codigos_router
-from routes.asignar import router as asignar_router
 from routes.admin_panel import router as admin_panel_router
 from routes.reporte_semanal import router as reporte_router
 from routes.reporte_formulario import router as reporte_formulario_router
@@ -37,7 +36,6 @@ app.include_router(lavado_router)
 app.include_router(empleados_router)
 app.include_router(vehiculos_router)
 app.include_router(codigos_router)
-app.include_router(asignar_router)
 app.include_router(admin_panel_router)
 app.include_router(reporte_router)
 app.include_router(reporte_formulario_router)
